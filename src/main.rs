d56789f3e810162bbb24db55f25691abb775fbb2
Amol Grover <frextrite@gmail.com>
2024-03-10 23:00:32 +0530

    add support for echo command

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,52 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::str;
 
 const ADDR: &str = "127.0.0.1:6379";
+
+enum Command<'a> {
+    Ping,
+    Echo(&'a str),
+}
+
+fn handle_command(command: Command, stream: &mut TcpStream) -> std::io::Result<()> {
+    match command {
+        Command::Ping => write!(stream, "+PONG\r\n")?,
+        Command::Echo(data) => write!(stream, "${}\r\n{}\r\n", data.len(), data)?,
+    }
+    Ok(())
+}
+
+fn parse_message(message: &str) -> Option<Command> {
+    let segments = message.split("\r\n").collect::<Vec<_>>();
+    if segments[2].eq_ignore_ascii_case("ping") {
+        return Some(Command::Ping);
+    } else if segments[2].eq_ignore_ascii_case("echo") {
+        return Some(Command::Echo(segments[4]));
+    }
+
+    None
+}
 
 fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let mut buf = [0; 32];
     loop {
         match stream.read(&mut buf) {
             Ok(bytes_read) => {
+                let message = str::from_utf8(&buf[..bytes_read]).unwrap();
                 println!(
-                    "INFO: read {} bytes from connection {:?}",
+                    "INFO: read message {:?} with {} bytes from connection {:?}",
+                    message,
                     bytes_read,
                     stream.peer_addr()
                 );
                 if bytes_read == 0 {
                     break;
                 }
-                write!(stream, "+PONG\r\n").unwrap();
+                match parse_message(message) {
+                    Some(command) => handle_command(command, &mut stream)?,
+                    None => eprintln!("ERROR: failed to parse message {:?}", message),
+                }
             }
             Err(error) => {
                 eprintln!(
@@ -24,6 +54,10 @@ fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
                     stream.peer_addr(),
                     error
                 );
+                match error.kind() {
+                    std::io::ErrorKind::Interrupted => continue,
+                    _ => break,
+                }
             }
         }
     }

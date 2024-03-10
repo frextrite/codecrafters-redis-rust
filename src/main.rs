f87d4f3dae40d63122c72c5f79fe6d5cabb4a4bc
Amol Grover <frextrite@gmail.com>
2024-03-10 13:48:29 +0530

    respond to multiple ping requests on same connection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 const ADDR: &str = "127.0.0.1:6379";
@@ -15,7 +15,29 @@ fn main() {
                     "INFO: accepted incoming connection from {:?}",
                     stream.peer_addr()
                 );
-                write!(stream, "+PONG\r\n").unwrap();
+                let mut buf = [0; 32];
+                loop {
+                    match stream.read(&mut buf) {
+                        Ok(bytes_read) => {
+                            println!(
+                                "INFO: read {} bytes from connection {:?}",
+                                bytes_read,
+                                stream.peer_addr()
+                            );
+                            if bytes_read == 0 {
+                                break;
+                            }
+                            write!(stream, "+PONG\r\n").unwrap();
+                        }
+                        Err(error) => {
+                            eprintln!(
+                                "ERROR: failed to read from connection {:?} with error {:?}",
+                                stream.peer_addr(),
+                                error
+                            );
+                        }
+                    }
+                }
             }
             Err(error) => {
                 eprintln!(

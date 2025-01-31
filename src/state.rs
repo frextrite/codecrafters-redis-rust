306f5eb781da5ade6a3fedad63887971419c7581
Amol Grover <frextrite@gmail.com>
2025-01-31 18:16:24 +0530

    Simplify state management

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,6 @@
 use std::{
     collections::HashMap,
-    sync::{Arc, Condvar, Mutex},
+    sync::{Arc, Condvar, Mutex, RwLock},
     thread::{self, JoinHandle},
     time::{Duration, Instant},
 };
@@ -9,24 +9,23 @@ type BinaryData = Vec<u8>;
 type KeyType = BinaryData;
 type Expiry = Option<Instant>;
 type ValueType = (BinaryData, Expiry);
+type Store = RwLock<HashMap<KeyType, ValueType>>;
+type StopCondition = (Mutex<bool>, Condvar);
+
+const SLEEP_TIMEOUT: Duration = Duration::from_secs(60);
 
 pub struct ExpiringHashMap {
-    store: Arc<Mutex<HashMap<KeyType, ValueType>>>,
-    gc_stop_cv: Arc<Condvar>,
-    gc_stop_requested: Arc<Mutex<bool>>,
+    store: Arc<Store>,
+    gc_stop: Arc<StopCondition>,
     gc_handle: Option<JoinHandle<()>>,
 }
 
 impl Drop for ExpiringHashMap {
     fn drop(&mut self) {
         if let Some(handle) = self.gc_handle.take() {
-            {
-                let mut gc_stop_required = self.gc_stop_requested.lock().unwrap();
-                if !*gc_stop_required {
-                    *gc_stop_required = true;
-                    self.gc_stop_cv.notify_one();
-                }
-            }
+            let (gc_stop_requested, gc_stop_cv) = &*self.gc_stop;
+            *gc_stop_requested.lock().unwrap() = true;
+            gc_stop_cv.notify_all();
             _ = handle.join();
         }
     }
@@ -40,59 +39,73 @@ impl Default for ExpiringHashMap {
 
 impl ExpiringHashMap {
     pub fn new() -> Self {
-        let store = Arc::new(Mutex::new(HashMap::new()));
-        let gc_stop_cv = Arc::new(Condvar::new());
-        let gc_stop_requested = Arc::new(Mutex::new(false));
-        ExpiringHashMap {
-            store: store.clone(),
-            gc_stop_cv: gc_stop_cv.clone(),
-            gc_stop_requested: gc_stop_requested.clone(),
-            gc_handle: Some(thread::spawn(move || {
-                let sleep_timeout = Duration::from_secs(5); // 5mins
-                loop {
-                    {
-                        let stop_guard = gc_stop_requested.lock().unwrap();
-                        let stop_requested = gc_stop_cv
-                            .wait_timeout(stop_guard, sleep_timeout)
-                            .unwrap()
-                            .0;
-                        if *stop_requested {
-                            println!("INFO: stopping GC thread");
-                            break;
-                        }
-                    }
+        let mut map = ExpiringHashMap {
+            store: Arc::new(RwLock::new(HashMap::new())),
+            gc_stop: Arc::new((Mutex::new(false), Condvar::new())),
+            gc_handle: None,
+        };
 
-                    let num_expired_keys;
-                    {
-                        let mut store = store.lock().unwrap();
-                        let current_time = Instant::now();
-                        let mut expired_keys = vec![];
-                        for (key, (_, ttl)) in store.iter() {
-                            if let Some(ttl) = ttl {
-                                if ttl < &current_time {
-                                    expired_keys.push(key.clone());
-                                }
-                            }
-                        }
-                        num_expired_keys = expired_keys.len();
-                        for key in expired_keys.into_iter() {
-                            store.remove(&key);
-                        }
-                    }
+        map.start_gc_thread();
 
-                    if num_expired_keys > 0 {
-                        println!("INFO: cleaned up {} expired keys", num_expired_keys);
-                    }
-                }
-            })),
+        map
+    }
+
+    fn start_gc_thread(&mut self) {
+        let store = self.store.clone();
+        let gc_stop = self.gc_stop.clone();
+
+        self.gc_handle = Some(thread::spawn(move || {
+            Self::gc_thread_function(store, gc_stop);
+        }));
+    }
+
+    fn gc_thread_function(store: Arc<Store>, gc_stop: Arc<StopCondition>) {
+        loop {
+            let (gc_stop_requested, _) = &*gc_stop;
+            let stop_requested = *gc_stop_requested.lock().unwrap();
+            if stop_requested {
+                println!("INFO: stopping GC thread");
+                break;
+            }
+
+            let num_expired_keys = Self::cleanup_expired_keys(store.clone());
+
+            if num_expired_keys > 0 {
+                println!("INFO: cleaned up {} expired keys", num_expired_keys);
+            }
+
+            let (gc_stop_requested, gc_stop_cv) = &*gc_stop;
+            let _ = gc_stop_cv.wait_timeout(gc_stop_requested.lock().unwrap(), SLEEP_TIMEOUT);
         }
     }
 
-    pub fn get(&mut self, key: &[u8]) -> Option<Vec<u8>> {
-        let mut store = self.store.lock().unwrap();
+    fn cleanup_expired_keys(store: Arc<Store>) -> usize {
+        let mut store = store.write().unwrap();
+
+        let current_time = Instant::now();
+        let mut num_expired_keys = 0;
+
+        store.retain(|_, (_, expiry)| {
+            if let Some(expiry) = expiry.as_ref() {
+                if expiry < &current_time {
+                    num_expired_keys += 1;
+                    return false;
+                }
+            }
+
+            true
+        });
+
+        num_expired_keys
+    }
+
+    pub fn get(&self, key: &[u8]) -> Option<BinaryData> {
+        let store = self.store.write().unwrap();
+
         if let Some((_, Some(ttl))) = store.get(key) {
             if ttl < &Instant::now() {
-                store.remove(key);
+                drop(store);
+                self.store.write().unwrap().remove(key);
                 return None;
             }
         }
@@ -100,11 +113,14 @@ impl ExpiringHashMap {
         store.get(key).map(|(value, _)| value).cloned()
     }
 
-    pub fn set(&mut self, key: &[u8], value: &[u8], expiry: Option<Duration>) {
-        let ttl = expiry.map(|duration| Instant::now() + duration);
-        self.store
-            .lock()
-            .unwrap()
-            .insert(key.to_vec(), (value.to_vec(), ttl));
+    pub fn set(&self, key: &[u8], value: &[u8], expiry: Option<Duration>) {
+        let ttl = Self::calculate_ttl(expiry);
+        let mut store = self.store.write().unwrap();
+
+        store.insert(key.to_vec(), (value.to_vec(), ttl));
+    }
+
+    fn calculate_ttl(expiry: Option<Duration>) -> Option<Instant> {
+        expiry.and_then(|duration| Instant::now().checked_add(duration))
     }
 }
